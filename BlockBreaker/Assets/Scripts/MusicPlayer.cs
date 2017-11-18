75da3a8bc9c9380a19ab5d46567bd9b899af4a80
Sung Ho Kim <likeleon@gmail.com>
2017-11-18 09:20:54 +0900

    음악 중복 재생 막음

--- BlockBreaker/Assets/Scripts/MusicPlayer.cs
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -2,8 +2,17 @@
 
 public class MusicPlayer : MonoBehaviour
 {
+    private static MusicPlayer _instance;
+
     void Start()
     {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
         DontDestroyOnLoad(gameObject);
     }
 
