2b94a7321075bdedc23a11f431eec936ba5e0864
Sung Ho Kim <likeleon@gmail.com>
2017-11-18 10:52:01 +0900

    LoseCollider

---
+++ BlockBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+}
