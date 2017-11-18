741f394f467016f856d376f73c1fa6c616e8a568
Sung Ho Kim <likeleon@gmail.com>
2017-11-18 09:07:30 +0900

    복구

--- BlockBreaker/Assets/Scripts/LevelManager.cs
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        SceneManager.LoadScene(name);
+    }
 
-	public void LoadLevel(string name){
-		Debug.Log ("New Level load: " + name);
-		Application.LoadLevel (name);
-	}
-
-	public void QuitRequest(){
-		Debug.Log ("Quit requested");
-		Application.Quit ();
-	}
-
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
 }
