ea83458b87596c91adc11d043152c6e253ddf023
Sung Ho Kim <likeleon@gmail.com>
2017-11-19 22:09:03 +0900

    Physics material Movement by mouse

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    void Update()
+    {
+        var mouseWorldPosX = (Input.mousePosition.x / Screen.width) * 16.0f;
+        mouseWorldPosX = Mathf.Clamp(mouseWorldPosX, 0.5f, 15.5f);
+        transform.position = new Vector3(mouseWorldPosX, transform.position.y);
+    }
+}
