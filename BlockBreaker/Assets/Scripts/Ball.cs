2bc28cd73271d8ec17653aa97484162c12d2c14e
Sung Ho Kim <likeleon@gmail.com>
2017-11-19 22:47:12 +0900

    Launching ball on mouse click

---
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Paddle Paddle;
+
+    private bool _isStarted;
+    private Vector3 _paddleToBall;
+
+    void Start()
+    {
+        _paddleToBall = transform.position - Paddle.transform.position;
+    }
+    
+    void Update()
+    {
+        if (!_isStarted)
+        {
+            transform.position = Paddle.transform.position + _paddleToBall;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+
+                _isStarted = true;
+            }
+        }
+    }
+}
