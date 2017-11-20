4f51671e991816586eccae4ffe58f79670789d50
Sung Ho Kim <likeleon@gmail.com>
2017-11-20 22:26:18 +0900

    Brick prefabs

---
+++ BlockBreaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    private int _timesHit;
+
+    public int MaxHits;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        ++_timesHit;
+    }
+}
