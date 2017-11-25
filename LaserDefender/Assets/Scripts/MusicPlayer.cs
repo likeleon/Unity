df63cfe35b4a46e409a8af606f6ef7b47be014ff
Sung Ho Kim <likeleon@gmail.com>
2017-11-26 00:28:29 +0900

    New project: Lader Defender Import menu system

---
+++ LaserDefender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer _instance;
+
+    void Start()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+    }
+}
