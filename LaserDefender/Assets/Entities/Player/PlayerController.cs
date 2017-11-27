0408a1c8a3d52d6aa8e42cfb7280e91870677c28
Sung Ho Kim <likeleon@gmail.com>
2017-11-28 00:37:28 +0900

    Spawning projectiles

--- LaserDefender/Assets/Entities/Player/PlayerController.cs
+++ LaserDefender/Assets/Entities/Player/PlayerController.cs
@@ -4,6 +4,9 @@ public class PlayerController : MonoBehaviour
 {
     public float MovementSpeed = 15.0f;
     public float Padding = 1f;
+    public GameObject Projectile;
+    public float ProjectileSpeed;
+    public float FiringRate = 0.2f;
 
     private float _minX;
     private float _maxX;
@@ -17,6 +20,15 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating(nameof(Fire), 0.000001f, FiringRate);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke(nameof(Fire));
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * MovementSpeed * Time.deltaTime;
@@ -28,5 +40,11 @@ public class PlayerController : MonoBehaviour
 
         var newX = Mathf.Clamp(transform.position.x, _minX, _maxX);
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+
+    private void Fire()
+    {
+        var beam = Instantiate(Projectile, transform.position, Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, ProjectileSpeed, 0);
     }
 }
