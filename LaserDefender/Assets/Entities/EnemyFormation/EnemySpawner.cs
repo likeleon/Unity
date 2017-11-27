328942cd20fcf3037051a835184ec8e9bae1ccfb
Sung Ho Kim <likeleon@gmail.com>
2017-11-27 23:12:56 +0900

    Moving the enemy formation

--- LaserDefender/Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ LaserDefender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -1,11 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject EnemyPrefab;
 
+    public float Width = 10f;
+    public float Height = 5f;
+    public float MoveSpeed = 5.0f;
+
+    private bool _movingRight = true;
+    private float _minX;
+    private float _maxX;
+
     private void Start()
     {
+        var distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+        _minX = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distanceToCamera)).x;
+        _maxX = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, distanceToCamera)).x;
+
         foreach (Transform child in transform)
         {
             var enemy = Instantiate(EnemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
@@ -13,8 +26,31 @@ public class EnemySpawner : MonoBehaviour
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height));
+    }
+
     private void Update()
     {
+        if (_movingRight)
+        {
+            transform.position += Vector3.right * MoveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += Vector3.left * MoveSpeed * Time.deltaTime;
+        }
 
+        float formationRight = transform.position.x + (0.5f * Width);
+        float formationLeft = transform.position.x - (0.5f * Width);
+        if (formationLeft < _minX)
+        {
+            _movingRight = true;
+        }
+        else if (formationRight > _maxX)
+        {
+            _movingRight = false;
+        }
     }
 }
