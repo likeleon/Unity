d12cc2b81ad92e451404006bdbfca2f86de03538
Sung Ho Kim <likeleon@gmail.com>
2017-11-27 21:43:13 +0900

    Creating the enemies & formation

---
+++ LaserDefender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            var enemy = Instantiate(EnemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
