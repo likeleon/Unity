8bbea7e3af28df69d2fcb555edb8f43b25210598
Sung Ho Kim <likeleon@gmail.com>
2017-11-28 22:57:29 +0900

    Shooting enemies

---
+++ LaserDefender/Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float Health = 150f;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        var projectile = collider.gameObject.GetComponent<Projectile>();
+        if (projectile == null)
+        {
+            return;
+        }
+
+        projectile.Hit();
+
+        Health -= projectile.Damage;
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LaserDefender/Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float Damage = 100f;
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+}
