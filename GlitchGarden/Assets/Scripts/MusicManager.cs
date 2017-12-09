f1a30285c2d8ac920124f2e42d52920bbe274cc9
Sung Ho Kim <likeleon@gmail.com>
2017-12-09 12:16:32 +0900

    Persistent music manager

---
+++ GlitchGarden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] LevelMusicChanges;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (level >= LevelMusicChanges.Length)
+        {
+            return;
+        }
+
+        var thisLevelMusic = LevelMusicChanges[level];
+        if (thisLevelMusic == null)
+        {
+            return;
+        }
+
+        _audioSource.clip = thisLevelMusic;
+        _audioSource.loop = true;
+        _audioSource.Play();
+    }
+}
