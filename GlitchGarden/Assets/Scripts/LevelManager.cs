ae04f95ddaf4f1b20e0deedeb64a563bdcb00e7b
Sung Ho Kim <likeleon@gmail.com>
2017-12-09 11:04:49 +0900

    Splash screen

---
+++ GlitchGarden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public float AutoLoadNextLevelDelay;
+
+    private void Start()
+    {
+        Invoke(nameof(LoadNextLevel), AutoLoadNextLevelDelay);
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
