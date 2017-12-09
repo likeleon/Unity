1a9d464106c93ca1388a3448aeae7a1b6185ab3b
Sung Ho Kim <likeleon@gmail.com>
2017-12-09 14:48:15 +0900

    Fade in panel

---
+++ GlitchGarden/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour 
+{
+    public float FadeInTime;
+
+    private Image _fadePanel;
+    private Color _currentColor = Color.black;
+
+    private void Start()
+    {
+        _fadePanel = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad < FadeInTime)
+        {
+            float alphaChange = Time.deltaTime / FadeInTime;
+            _currentColor.a -= alphaChange;
+            _fadePanel.color = _currentColor;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
