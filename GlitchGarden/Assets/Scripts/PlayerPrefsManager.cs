b76547e3ca3fb41258e3cdd00cead2400d009c35
Sung Ho Kim <likeleon@gmail.com>
2017-12-09 16:43:12 +0900

    PlayerPrefsManager

---
+++ GlitchGarden/Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    const string MasterVolumeKey = "master_volume";
+    const string DifficultyKey = "difficulty";
+    const string LevelKey = "level_unlocked_";
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume > 0f && volume < 1f)
+        {
+            PlayerPrefs.SetFloat(MasterVolumeKey, volume);
+        }
+        else
+        {
+            Debug.LogError("Master volume out of range");
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MasterVolumeKey);
+    }
+
+    public static void UnlockLevel(int level)
+    {
+        if (level < SceneManager.sceneCountInBuildSettings)
+        {
+            PlayerPrefs.SetInt($"{LevelKey}{level}", 1);
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level not in build order");
+        }
+    }
+
+    public static bool IsLevelUnlocked(int level)
+    {
+        if (level < SceneManager.sceneCountInBuildSettings)
+        {
+            return PlayerPrefs.GetInt($"{LevelKey}{level}") == 1;
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level not in build order");
+            return false;
+        }
+    }
+
+    public static void SetDifficulty(float difficulty)
+    {
+        if (difficulty >= 0f && difficulty <= 1f)
+        {
+            PlayerPrefs.SetFloat(DifficultyKey, difficulty);
+        }
+        else
+        {
+            Debug.LogError("Difficulty out of range");
+        }
+    }
+
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetFloat(DifficultyKey);
+    }
+}
