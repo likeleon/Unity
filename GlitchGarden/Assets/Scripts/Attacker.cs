7daf0379490313dd9d094d623f7c1667fa11f994
Sung Ho Kim <likeleon@gmail.com>
2017-12-19 00:03:58 +0900

    Moving attackers

---
+++ GlitchGarden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(-1f, 1.5f)]
+    public float WalkSpeed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * WalkSpeed * Time.deltaTime);
+    }
+}
